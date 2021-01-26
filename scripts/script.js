92cc52081ffd60dacaf261dcec03e0383cafcb18
Jarred Herrington <herringtonjc@yahoo.com>
2021-01-26 00:28:22 -0600

    Moved the function to display book info to prototypal

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,21 @@
+let myLibrary = [];
+
+
 function Book(title, author, numberOfPages, haveRead) {
     this.title = title
     this.author = author
     this.numberOfPages = numberOfPages
     this.haveRead = haveRead
+};
 
-    this.info = function() {
-        return(`${title} by ${author}, ${numberOfPages} pages, ${haveRead}`)
-    }
+Book.prototype.info = function() {
+    return `${this.title} by ${this.author}, ${this.numberOfPages} pages, ${this.haveRead}`;
+};
+
+function addBookToLibrary(bookToAdd) {
+    myLibrary.push(bookToAdd)
+};
+
+function displayBook() {
+
 }
